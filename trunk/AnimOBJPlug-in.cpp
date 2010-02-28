e83485606af825942c96ff00ffac13c727d06236
saggitasaggita@yahoo.com <saggitasaggita@yahoo.com@881fa37e-242c-11df-8901-e16673284e38>
2010-02-28 05:55:50 +0000

    Initial version (v0.1)

---
+++ trunk/AnimOBJPlug-in.cpp
@@ -0,0 +1,49 @@
+#include <maya/MFnPlugin.h>
+#include <maya/MPxCommand.h>
+#include <maya/MGlobal.h>
+
+#include "AnimOBJNode.h"
+#include "AnimOBJ_LoadCommand.h"
+#include <sstream>
+
+MStatus initializePlugin(MObject obj)
+{
+	MStatus   status;
+	MFnPlugin plugin(obj, "AnimOBJ", "0.1", "Any");
+
+	status = plugin.registerNode("AnimOBJNode", AnimOBJNode::id,
+						 AnimOBJNode::creator, AnimOBJNode::initialize);
+
+	plugin.registerCommand( "AnimOBJ_LoadCommand", AnimOBJ_LoadCommand::creator );
+
+	MGlobal::executeCommand( "source AnimOBJMain.mel" );
+    MGlobal::executeCommand( "AnimOBJMain_initialize" );
+
+	if (!status) {
+		status.perror("registerNode");
+		return status;
+	}
+
+	return status;
+}
+
+MStatus uninitializePlugin(MObject obj)
+{
+	MStatus	  status;
+	MFnPlugin plugin(obj);
+
+	status = plugin.deregisterNode(AnimOBJNode::id);
+	
+	plugin.deregisterCommand( "AnimOBJ_LoadCommand" );
+	
+	if (!status) {
+		status.perror("deregisterNode");
+		return status;
+	}
+
+	return status;
+}
+
+
+
+
